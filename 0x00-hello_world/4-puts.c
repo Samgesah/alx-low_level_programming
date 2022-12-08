59ddae6bb24df33301acc2b75a2606afd61c4f47
Samgesah <samgesa@yahoo.com>
2022-12-08 15:16:03 +0300

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,9 @@
  * Return: Always (0) success
  */
 
-int puts(const char *str);
 int main(void)
 {
-	char Mystr[] = "Programming is like building a multilingual puzzle";
+	char Mystr[] = "Programming is like building a multilingual puzzle;
 
 	puts(Mystr);
 	return (0);
