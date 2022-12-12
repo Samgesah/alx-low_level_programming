10f3a2c0a7d4dd8fd0dbd2f4a40e09415b9e3c07
Samgesah <samgesa@yahoo.com>
2022-12-12 22:27:32 +0300

    Three distinct numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,23 +11,23 @@ int main(void)
 
 	for (a = '0'; a < '9'; a++)
 	{
-		for (b = a + 1; b <= '9'; b++)
-		{
-			for (c = b + 1; c <= '9'; c++)
-			{
-			 if ((b != a) != c)
-			 {
-				 	putchar(a);
-					putchar(b);
-					putchar(c);
+	for (b = a + 1; b <= '9'; b++)
+	{
+	for (c = b + 1; c <= '9'; c++)
+	{
+	if ((b != a) != c)
+	{
+		putchar(a);
+		putchar(b);
+		putchar(c);
 
-					if (a == '7' && b == '8' && c == '9')
-						continue;
-					putchar(',');
-					putchar(' ');
-				}
-			}
+		if (a == '1' && b == '2' && c == '3')
+			continue;
+		putchar(',');
+		putchar(' ');
 		}
+	}
+	}
 	}
 	putchar('\n');
 	return (0);
