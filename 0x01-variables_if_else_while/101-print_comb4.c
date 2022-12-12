c05e3a68de6943cbbab8626e3fa220062f060720
Samgesah <samgesa@yahoo.com>
2022-12-12 22:08:17 +0300

    three distinct digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,19 +9,19 @@ int main(void)
 {
 	int a, b, c;
 
-	for (a = '0'; a < 9; a++)
+	for (a = '0'; a < '9'; a++)
 	{
 		for (b = a + 1; b <= '9'; b++)
 		{
 			for (c = b + 1; c <= '9'; c++)
 			{
-				if ((b != a) != c)
-				{
-					putchar(a);
+			 if ((b != a) != c)
+			 {
+				 	putchar(a);
 					putchar(b);
 					putchar(c);
 
-					if (a == '7' && b == '8')
+					if (a == '7' && b == '8' && c == '9')
 						continue;
 					putchar(',');
 					putchar(' ');
