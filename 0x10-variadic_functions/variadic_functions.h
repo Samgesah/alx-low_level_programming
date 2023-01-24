3a53e5f13c64a0b2e8e8445e76a654c4aa98debc
Samgesah <samgesa@yahoo.com>
2023-01-24 22:34:40 +0300

    All header files

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,22 @@
 #ifndef VARIADIC_H
 #define VARIADIC_H
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * struct args_t - contain the type and the function.
+ * @format: the format to print
+ * @function: the functions that prints
+ *
+ * Description: contains the type and the format to print.
+ */
+
+typedef struct args_t
+{
+	char *format;
+	void (*function)();
+} args_t;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
